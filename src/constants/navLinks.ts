d000b28da83512193991178ec2d33cfa81b09a5d
Josue Florian <david.lopez@orchardtek.com>
2025-09-09 16:11:25 -0600

    fix: hidding projects page, by the moment.

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -3,7 +3,7 @@ import { NavItem } from "@/types/NavLinks";
 export const navLinks: NavItem[] = [
     { href: '/', label: 'Inicio' },
     { href: '/about', label: 'Sobre mí' },
-    { href: '/projects', label: 'Proyectos', badge: '5' },
+    // { href: '/projects', label: 'Proyectos', badge: '5' },
     { href: '/experience', label: 'Experiencia' },
     // { href: '/blog', label: 'Blog', badge: 'New' },
     { href: 'https://drive.google.com/file/d/1ibR7wGjd8-C8qyHWksRyaGbVhpvgoP0g/view?usp=sharing', label: 'Resume', isExternal: true },
